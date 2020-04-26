02697830fd85a8f058ce0fd79dbf6f9fd666ec29
nnnkit <iankit17@gmail.com>
2020-04-26 23:05:13 +0530

    add prettier and change postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,19 @@
 const purgecss = [
-  "@fullhuman/postcss-purgecss",
+  '@fullhuman/postcss-purgecss',
   {
-    content: ["./components/**/*.js", "./pages/**/*.js"],
-    defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+    content: [
+      './components/**/*.js',
+      './pages/**/*.js',
+      './components/**/*.tsx',
+      './pages/**/*.tsx',
+      './components/**/*.jsx',
+      './pages/**/*.jsx',
+    ],
+    defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
   },
-];
+]
 
 module.exports = {
-  plugins: ["postcss-import", "tailwindcss", "autoprefixer"],
-  ...(process.env.NODE_ENV === "production" ? [purgecss] : []),
-};
+  plugins: ['postcss-import', 'tailwindcss', 'autoprefixer'],
+  ...(process.env.NODE_ENV === 'production' ? [purgecss] : []),
+}
